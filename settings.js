a6c483455407c592fbfb6ff5dc91ad09a53e4f2c
Demigodhack <98765258+Demigodhack@users.noreply.github.com>
2023-02-16 19:22:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,19 +7,19 @@ global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴�
 global.author = "𝙋𝙄𝙆𝘼"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼adhya✯𝘽𝙊𝙏"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
+global.waterMark = "demiCloud" // Your own watermark 
 global.headerText = "Anya by Pika" // text for header of menu         
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
+global.ownername = "demiCloud"   // Owner name //        
 global.ownernumber = ['19025101916'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : air.table" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "demigodgaming"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
 global.region = "India" // your country name
 global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
+global.district = "Banglore" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "air.table" // Your insta ID
 global.email = "demigod@hi2.in" // put your email here
